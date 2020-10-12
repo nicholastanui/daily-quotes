dece27c57ad51aa56b5488173e6c919b07c560e8
Nicholas Tanui <ntanui@safaricom.co.ke>
2020-10-12 23:31:56 +0300

    init setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AddQuoteComponent } from './home/add-quote/add-quote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddQuoteComponent
   ],
   imports: [
     BrowserModule,
